c8d313e6820b62dfa8b34e11881d2a96a6b62aa3
hiven <mail@hiven.com>
2019-03-16 07:57:54 +0700

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,29 +1,25 @@
-const stripe = require("stripe")("sk_test_Gl20U8POByRHxfJ9ozHOPotK")
-const Datastore = require('@google-cloud/datastore')
+var app = require('express')();
+var http = require('http').Server(app);
+var stripe = require('stripe')(
+  "fbUcgVE"
+);
+var bodyParser = require('body-parser');
 
-const ds = Datastore({
-  projectId: "stripeexample-187817",
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-exports.cloudCharge = function cloudCharge(req, res) {
-  const { stripeToken, productId } = req.body
+//get token from body
 
-  const key = ds.key(["product", parseInt(productId, 10)])
-  ds.get(key, (err, product) => {
-    stripe.charges.create({
-      amount: product.price,
-      currency: "usd",
-      source: stripeToken,
-      description: `GIFs of ${product.set}`,
-    }, (err, charge) => {
-      if(err) {
-        console.error(err)
-        res.status(500).send(err)
-      }
-      else {
-        res.status(303).redirect(`https://henry-stripe.ngrok.io/thanks.html?charge=${charge.id}`)
-      }
-    })
-  })
-}
-
+//initiate a one-off charge for a customer
+exports.chargeCustomer = app.post("/", function chargeCustomer (req,res){
+  stripe.charges.create({
+    source: req.body.stripeToken,
+    currency: 'usd',
+    amount:999
+  },function(err, charge) {
+    if(err) {
+      return res.send(JSON.stringify(err));
+    }
+    res.send(JSON.stringify(charge));
+  });
+});
